d36167a617ebda689ebed01cb854ce81c461bdc7
vlaskinmac <tesseractmaks@gmail.com>
2022-07-01 15:58:53 +0300

    Add blank start page

--- where_to_go_project/manage.py
+++ where_to_go_project/manage.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'where_to_go.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'where_to_go_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- where_to_go_project/where_to_go_project/wsgi.py
+++ where_to_go_project/where_to_go_project/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for where_to_go project.
+WSGI config for where_to_go_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'where_to_go.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'where_to_go_project.settings')
 
 application = get_wsgi_application()
