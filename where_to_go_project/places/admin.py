4347cb9cedcd479738b6d2cc62d72229f0114562
vlaskinmac <tesseractmaks@gmail.com>
2022-07-03 10:27:42 +0300

    Add model Places

--- where_to_go_project/places/admin.py
+++ where_to_go_project/places/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Place
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
--- where_to_go_project/places/models.py
+++ where_to_go_project/places/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
 # Create your models here.
-print("hello!")
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=250)
+    image = models.CharField(max_length=250, null=True, blank=True)
+    description_short = models.CharField(max_length=250)
+    description_long = models.CharField(max_length=250)
+    lng = models.FloatField()
+    lat = models.FloatField()
+
+    def __str__(self):
+        return f"{self.title} {self.description_short} {self.description_long}"
