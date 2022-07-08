92ccb96bb802203593df8a0fe55106209e3a1038
vlaskinmac <tesseractmaks@gmail.com>
2022-07-08 11:25:51 +0300

    Fixed STATIC_URL

--- where_to_go_project/where_to_go_project/settings.py
+++ where_to_go_project/where_to_go_project/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/uploads_static/'
-STATICFILES_DIRS = '/uploads_static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = ['/uploads_static/']
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # место хранения на сервере
 
 MEDIA_URL = '/uploads/' # внешний адрес для раздачи браузеру
