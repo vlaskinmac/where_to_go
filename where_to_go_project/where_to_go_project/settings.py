51d658f2cc99349fd792f13a521c98191c717253
vlaskinmac <tesseractmaks@gmail.com>
2022-07-08 11:15:59 +0300

    Fixed STATICFILES_DIRS

--- where_to_go_project/where_to_go_project/settings.py
+++ where_to_go_project/where_to_go_project/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/uploads_static/'
-
+STATICFILES_DIRS = '/uploads_static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # место хранения на сервере
 
 MEDIA_URL = '/uploads/' # внешний адрес для раздачи браузеру
