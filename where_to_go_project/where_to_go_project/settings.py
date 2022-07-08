49bcd5ab9091c3d6eea2b62a963ee08a090bc66f
vlaskinmac <tesseractmaks@gmail.com>
2022-07-08 11:12:10 +0300

    Fixed STATIC_ROOT

--- where_to_go_project/where_to_go_project/settings.py
+++ where_to_go_project/where_to_go_project/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/uploads_static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # место хранения на сервере
